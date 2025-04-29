443d80471c907e171e0745fc4140f1648ebc876f
yasmeentamer <168345403+yasmeentamer@users.noreply.github.com>
2025-04-29 23:00:43 +0300

    Add files via upload

--- src/main/java/com/example/dbcinema/SQLServerConnect.java
+++ src/main/java/com/example/dbcinema/SQLServerConnect.java
@@ -1,16 +1,25 @@
-package com.example.dbcinema;
+package org.example.cinemadb;
 
-import java.sql.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLServerConnect {
-    public static void main(String[] args) {
-        String connectionUrl = "jdbc:sqlserver://Jarvis:1433;databaseName=Cinemadb;user=User1;password=root;encrypt=true;trustServerCertificate=true";
+    private static final String URL = "jdbc:sqlserver://LAPTOP-859490JA:37233;databaseName=Project;encrypt=true;trustServerCertificate=true";
+    private static final String USER = "User1";
+    private static final String PASS = "root";
 
-        try (Connection conn = DriverManager.getConnection(connectionUrl)) {
-            System.out.println("Connected to SQL Server successfully.");
+    public SQLServerConnect() {
+    }
 
-        } catch (SQLException e) {
-            e.printStackTrace();
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+            System.out.println("connection established!");
+            return DriverManager.getConnection("jdbc:sqlserver://LAPTOP-859490JA:37233;databaseName=Project;encrypt=true;trustServerCertificate=true", "User1", "root");
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("Database driver not found", e);
         }
     }
 }
